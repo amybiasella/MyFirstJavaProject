3bddd500e69de7a4b12e601b90248c2f1fbdbba6
biasella <biasella@broadinstitute.org>
2017-08-24 14:44:31 -0400

    Tweaked the output text to Hello to the world

--- MyProgram.java
+++ MyProgram.java
@@ -2,6 +2,6 @@ public class MyProgram {
 
     public static void main(String[] args) {
         //Prints Hello World in the terminal window
-        System.out.println("Hello World!");
+        System.out.println("Hello to the World!");
     }
 }
